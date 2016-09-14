82664dbabadeb6db19b1e69aa9cdf7baddbce3e3
Imran Uddin <imran.uddin.ece@gmail.com>
2016-09-14 01:28:43 -0400

    1 file at a time?

--- milestone1.4/include/bmp_file.h
+++ milestone1.4/include/bmp_file.h
@@ -1,62 +1,32 @@
 #ifndef BMP_FILE_H
 #define BMP_FILE_H
 
-#include <string>
 #include <iostream>
 #include <fstream>
 #include <stdint.h>
 #include <vector>
 
-class bmp_file
-{
+class bmp_file {
     public:
-        bmp_file();
-        virtual ~bmp_file();
+        bmp_file();                                     /**< Default constructor */
+        virtual ~bmp_file();                            /**< Default destructor */
+        bmp_file(char* filepath);                      /**< Creates an instance of bmp_file containing the specified file */
 
-        bmp_file(std::string filepath);
-        void writeToNewFile(std::string filepath);
-        unsigned long getFileSize();
-        unsigned long getStartOfBitmap();
-        unsigned long getWidth();
-        unsigned long getHeight();
-        unsigned long getNumberOfColorsInPalette();
-        void printData();
+        void writeToNewFile(char* filepath);           /**< Writes the data in the bmp_file instance to the specified file */
+        unsigned long getFileSize();                    /**< Extracts the file size from the bitmap header */
+        unsigned long getStartOfBitmap();               /**< Extracts the image offset from the bitmap header */
+        unsigned long getWidth();                       /**< Extracts the image width from the bitmap header */
+        unsigned long getHeight();                      /**< Extracts the image height from the bitmap header */
+        unsigned long getNumberOfColorsInPalette();     /**< Extracts the bits/px value from the bitmap header */
+        void printData();                               /**< Outputs header data to the console using the std::cout stream */
 
-    protected:
-
+        unsigned char getPixel(int index);              /**< Returns the pixel data located in the specified file byte */
+        void setPixel(int index, unsigned char byte);   /**< Writes a byte to the specified pixel */
     private:
-        unsigned long get32(int LSBindex);
-        std::vector<unsigned char> fileData;
-
+        unsigned long get32(int LSBindex);              /**< Extracts a 32-bit unsigned long value from fileData beginning with element LSBindex */
+        std::vector<unsigned char> fileData;            /**< Contains the bitmap data */
 };
 
 #endif // BMP_FILE_H
 
-//        unsigned long getHeaderSize();
-//        void setFileSize(unsigned long fileSize);
-//        void setStartOfBitmap(unsigned long startOfBitmap);
-//        void setHeaderSize( unsigned long headerSize);
-//        void setWidth(unsigned long width);
-//        void setHeight(unsigned long height);
-//
-//        unsigned short getBM();
-//
-//        unsigned short getPlanes();
-//        void setPlanes(unsigned short planes);
-//
-//        unsigned long getCompression();
-//        void setCompression(unsigned long compression);
-//
-//        unsigned long getCompressionSize();
-//        void setCompressionSize(unsigned long compressionSize);
-//
-//        unsigned long getHorizontalResolution();
-//        void setHorizontalResolution(unsigned long horizontalResolution);
-//
-//        unsigned long getVerticalResolution();
-//        void setVerticalResolution(unsigned long verticalResolution);
-//
-//        void setNumberOfColorsInPalette(unsigned long numberOfColorsInPalette);
-//
-//        unsigned long getImportantColors();
-//        void setImportantColors(unsigned long importantColors);
+/**< Graveyard */
