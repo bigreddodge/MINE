462638ab12937ed3509cb9aded8b629f7a72f40b
Imran Uddin <imran.uddin.ece@gmail.com>
2016-09-14 01:28:19 -0400

    Master cleanup
    Seriously cleaned up the code. Also, I believe I got the overlay
    working, but we'll have to test it more. Please notice getPixel() &
    setPixel(). Should be useful.
    Commented out code was moved to graveyard.txt.

--- milestone1.4/src/bmp_file.cpp
+++ milestone1.4/src/bmp_file.cpp
@@ -1,111 +1,93 @@
-#include "../include/bmp_file.h"
-
-#include <iostream>
-
-
-
 /** BMP Header Information
  * Note: All element indices are 0-based
  * File Header:
- *   Filesize: bytes 2-5
+ *   File size: bytes 2-5
  *   Offset: bytes 10-13
  * DIB Header:
  *   Width: bytes 18-21
  *   Height: bytes 22-25
  */
 
-bmp_file::bmp_file()
-{
-    //ctor
-}
+#include "../include/bmp_file.h"
+#include <iostream>
 
-bmp_file::bmp_file(std::string filepath)
-{
+bmp_file::bmp_file(){/**< Default Constructor */}
+bmp_file::~bmp_file(){/**< Default Destructor */}
+
+/**< Creates an instance of bmp_file containing the specified file */
+bmp_file::bmp_file(char* filepath) {
     std::ifstream infile;
-    infile.open(filepath.c_str(), std::ifstream::in | std::ifstream::binary);
-
+    infile.open(filepath, std::ifstream::in | std::ifstream::binary);
+    if (!infile){
+        std::cout << "\nFile " << filepath << " not found.";
+        return;
+    } else {
+        std::cout << "\nOpening " << filepath << "...";
+    }
     char * buffer = new char [1];
-    int counter = 0;
-    std::cout << "\n\n";
-    while (!infile.eof())
-    {
+    int counter = -1;
+    while (!infile.eof()) {
         infile.read(buffer, 1);
         fileData.push_back(*buffer);
         counter++;
-        //std::cout << buffer;
     }
     fileData.pop_back();
-    std::cout << "\n\nLoop counter: " << counter << std::endl;
+    std::cout << "\nBytes read: " << counter << std::endl;
 }
 
-bmp_file::~bmp_file()
-{
-    //dtor
-}
 
-void bmp_file::writeToNewFile(std::string filepath)
-{
+/**< Writes the data in the bmp_file instance to the specified file */
+void bmp_file::writeToNewFile(char* filepath) {
     std::ofstream outFile;
-    outFile.open(filepath.c_str(), std::ofstream::out | std::ofstream::binary);
-
+    outFile.open(filepath, std::ofstream::out | std::ofstream::binary);
+    if (!outFile){
+        std::cout << "\nUnable to write to " << filepath << ".";
+        return;
+    } else {
+        std::cout << "\nWriting " << filepath << "...";
+    }
     char * buffer = new char [1];
-    for (unsigned int i = 0; i < fileData.size(); i++)
-    {
+    for (unsigned int i = 0; i < fileData.size(); i++) {
         *buffer = fileData[i];
         outFile.write(buffer,1);
     }
 }
 
-void bmp_file::printData()
-{
-
-    std::cout << "\nFilesize: " << getFileSize();
-    std::cout << "\nOffset: " << getStartOfBitmap();
-    std::cout << "\nWidth: " << getWidth();
-    std::cout << "\nHeight: " << getHeight();
-    std::cout << "\nVector Size: " << fileData.size();
-    ///Pixel test:
-    int px = getStartOfBitmap();
-    std::cout << "\n\n";
-    for (int c = 0; c < 3; ++c){
-        for (int i = 0; i < 8; ++i){
-            std::cout << ((fileData[px+c] >> i) & 0x01);
-        }
-        std::cout << "\n";
-    }
+/**< Returns the pixel data located in the specified file byte */
+unsigned char bmp_file::getPixel(int index){
+    return fileData[index];
 }
 
+/**< Writes a byte to the specified pixel */
+void bmp_file::setPixel(int index, unsigned char byte){
+    fileData[index] = byte;
+}
+
+/**< Outputs header data to the console using the std::cout stream */
+void bmp_file::printData() {
+    std::cout << "\nHeader data:";
+    std::cout << "\n  Filesize: " << getFileSize();
+    std::cout << "\n  Offset: " << getStartOfBitmap();
+    std::cout << "\n  Width: " << getWidth();
+    std::cout << "\n  Height: " << getHeight();
+    std::cout << "\n  Vector Size: " << fileData.size();
+}
+
+/**< The following functions extract data from the bitmap header */
 unsigned long bmp_file::getFileSize(){return get32(2);}
 unsigned long bmp_file::getStartOfBitmap(){return get32(10);}
 unsigned long bmp_file::getWidth(){return get32(18);}
 unsigned long bmp_file::getHeight(){return get32(22);}
-
-unsigned long bmp_file::get32(int LSBindex)
-{
-    return (unsigned long) (((unsigned long)fileData[LSBindex + 3]) << 24) | (((unsigned long)fileData[LSBindex + 2]) << 16) | (((unsigned long)fileData[LSBindex + 1]) << 8) | ((unsigned long)fileData[LSBindex]);
-}
-
 unsigned long bmp_file::getNumberOfColorsInPalette(){return get32(50);}
 
+/**< Extracts a 32-bit unsigned long value from fileData beginning with element LSBindex */
+unsigned long bmp_file::get32(int LSBindex){
+    unsigned long temp = 0;
+    for (int i = 3; i >=0; --i) {
+        temp = (fileData[LSBindex+i] | temp << 8);
+        temp |= fileData[LSBindex + i];
+    }
+    return temp;
+}
 
-//unsigned long bmp_file::getHeaderSize(){}
-//
-//unsigned short bmp_file::getPlanes(){}
-//void bmp_file::setPlanes(unsigned short planes){}
-//
-//unsigned long bmp_file::getCompression(){}
-//void bmp_file::setCompression(unsigned long compression){}
-//
-//unsigned long bmp_file::getCompressionSize(){}
-//void bmp_file::setCompressionSize(unsigned long compressionSize){}
-//
-//unsigned long bmp_file::getHorizontalResolution(){}
-//void bmp_file::setHorizontalResolution(unsigned long horizontalResolution){}
-//
-//unsigned long bmp_file::getVerticalResolution(){}
-//void bmp_file::setVerticalResolution(unsigned long verticalResolution){}
-//
-//void bmp_file::setNumberOfColorsInPalette(unsigned long numberOfColorsInPalette){}
-//
-//unsigned long bmp_file::getImportantColors(){}
-//void bmp_file::setImportantColors(unsigned long importantColors){}
+/**< Graveyard */
