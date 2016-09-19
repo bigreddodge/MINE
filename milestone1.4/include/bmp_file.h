2195d83c86a96b6cf3e6979ea6285944beaa10bc
Imran Uddin <vapidr1@yahoo.com>
2016-09-19 13:28:35 -0400

    MILESTONE 1 COMPLETE
    Fixed all conflicts

--- milestone1.4/include/bmp_file.h
+++ milestone1.4/include/bmp_file.h
@@ -10,23 +10,6 @@
 
 class bmp_file {
     public:
-<<<<<<< HEAD
-        bmp_file();                                     /**< Default constructor */
-        virtual ~bmp_file();                            /**< Default destructor */
-        bmp_file(char* filepath);                       /**< Creates an instance of bmp_file containing the specified file */
-
-        void writeToNewFile(char* filepath);            /**< Writes the data in the bmp_file instance to the specified file */
-        void histogram_equalization(char* filepath);    /**< Histogram equalizes the image and writes it to the file specified in filepath */
-        unsigned long getFileSize();                    /**< Extracts the file size from the bitmap header */
-        unsigned long getStartOfBitmap();               /**< Extracts the image offset from the bitmap header */
-        unsigned long getWidth();                       /**< Extracts the image width from the bitmap header */
-        unsigned long getHeight();                      /**< Extracts the image height from the bitmap header */
-        unsigned long getNumberOfColorsInPalette();     /**< Extracts the bits/px value from the bitmap header */
-        void printData();                               /**< Outputs header data to the console using the std::cout stream */
-
-        unsigned char getPixel(int index);              /**< Returns the pixel data located in the specified file byte */
-        void setPixel(int index, unsigned char byte);   /**< Writes a byte to the specified pixel */
-=======
         bmp_file();                                  /**< Default constructor */
         virtual ~bmp_file();                         /**< Default destructor */
 
@@ -37,7 +20,6 @@ class bmp_file {
         void writeToNewFile(char* filepath);
         /**< Histogram equalizes the image and writes it to the file specified in filepath */
         void histogram_equalization(char* filepath);
-
 
         /**< These functions Extract important values from the bitmap header */
         unsigned long getFileSize();
@@ -54,31 +36,30 @@ class bmp_file {
 
         /**< Writes a byte to the specified pixel */
         void setPixel(int index, unsigned char byte);
->>>>>>> refs/remotes/origin/final
 
     private:
-        /**< Extracts a 32-bit unsigned long from fileData element beginning with LSBindex */
+        /**< Extracts a 32-bit unsigned long value from fileData beginning with element LSBindex */
         unsigned long get32(int LSBindex);
-
-        /**< Contains the bitmap data */
-        std::vector<unsigned char> fileData;
+        std::vector<unsigned char> fileData;            /**< Contains the bitmap data */
 };
 
-/**< Counts color occurrences and calculates new color in Histogram Equalization */
-struct accumulator {
+/// used to count the occurrences of colors and then calculate the new color in Histogram Equalization
+struct accumulator
+{
     public:
-        accumulator(uint8_t _color) {
+        accumulator(uint8_t _color)
+        {
             color = _color;
-            counter = 0;}       /**< first occurrence of that color so set to 1 */
+            counter = 0;            ///first occurrence of that color so set to 1
+        }
         int counter;
-        int cCounter;           /**< cumulative count */
+        int cCounter;   ///cumulative count
         uint8_t color;
         uint8_t newColor;
-        double cPercent;        /**< cumulative percent */
+        double cPercent;       /// cumulative percent
+
+    private:
+
 };
 
-<<<<<<< HEAD
 #endif // BMP_FILE_H
-=======
-#endif // BMP_FILE_H
->>>>>>> refs/remotes/origin/final
